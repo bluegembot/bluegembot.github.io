c6fb3daacb94ef987e7056fa78f9bbacaca1c9fd
kaasl <lucas.kaas@hva.nl>
2024-11-17 23:47:45 +0100

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import './assets/style/main.css'
 
-import { createApp, provide } from 'vue'
+import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import { injectionKeys } from './injectionKeys'
-import { AuthenticationService } from './services/AuthenticationService'
 
 const app = createApp(App)
 
-app.provide(injectionKeys.AuthenticationServiceKey, new AuthenticationService())
 
 app.use(router).mount('#app')
