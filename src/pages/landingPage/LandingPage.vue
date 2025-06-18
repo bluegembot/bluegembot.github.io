e141900b55aea4188c55b63cdc5874b696938acd
Lucas Kaas <85673462+bluegembot@users.noreply.github.com>
2025-06-18 11:44:27 +0200

    re-added the video

--- src/pages/landingPage/LandingPage.vue
+++ src/pages/landingPage/LandingPage.vue
@@ -35,14 +35,15 @@
       </div>
     </section>
 
-<!--    <section id="video" ref="video" class="fade-section">-->
-<!--      <div class="content-wrapper">-->
-<!--        <h2>The latest BlueGemBot update</h2>-->
-<!--        <div class="video-container">-->
-<!--          <iframe src="https://www.youtube.com/embed/8gMYTLQiZ60?si=PaGFaB8x7IVL50mS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </section>-->
+    <section id="video" ref="video" class="fade-section">
+      <div class="content-wrapper">
+        <h2>The latest BlueGemBot update</h2>
+        <div class="video-container">
+          <iframe src="https://www.youtube.com/embed/8gMYTLQiZ60?si=PaGFaB8x7IVL50mS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+      </div>
+    </section>
+
 
     <section id="testimonials" ref="testimonials" class="fade-section">
       <div class="content-wrapper">
