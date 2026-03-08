f624d528931a4c8f0a698235183378223042e941
BlueGemBot <lucaskaasie@gmail.com>
2026-03-08 21:07:24 +0200

    New domain changes

--- src/config/environment.js
+++ src/config/environment.js
@@ -2,8 +2,8 @@ export const isDevelopment = import.meta.env.VITE_ENVIRONMENT === 'development';
 
 export const API_URL = isDevelopment
     ? 'http://localhost:3002'
-    : 'https://bluegembot.duckdns.org';
+    : 'https://api.bluegembot.com';
 
 export const WS_URL = isDevelopment
     ? 'ws://localhost:3002/ws'
-    : 'wss://bluegembot.duckdns.org/ws';
+    : 'wss://api.bluegembot.com/ws';
